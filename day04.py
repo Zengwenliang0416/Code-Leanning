d5d787361659bd7440abf467e6332dd061201412
Zengwenliang0416 <wenliang_zeng416@163.com>
2023-03-24 16:01:58 +0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   day04.py
    	new file:   day05.py

--- day04.py
+++ day04.py
@@ -21,4 +21,21 @@ def que2122():
     ordic = sorted(dic.items(),key=lambda x : x[1])
     print(ordic[m-1][0])
     return 0
-que2122()
+
+def que2117():
+    
+    n = int(input())
+    h = list(map(int,input().split()))
+    a = [[0 for j in range(6)] for i in range(len(h))]
+    def cut(x):
+        return int((x//2+1)**0.5//1)
+
+    count = 0
+    for i in range(len(h)):
+        while h[i]!=1:
+            h[i] = cut(h[i])
+            count+=1
+
+    return
+
+# print(que2117())
