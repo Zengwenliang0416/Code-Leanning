8da0049f04eb902fc9fae884cf1aca7fdb79be56
Zengwenliang0416 <wenliang_zeng416@163.com>
2023-03-24 15:25:04 +0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   day04.py
    
    Untracked files:
    	day05.py

--- day04.py
+++ day04.py
@@ -1,14 +1,23 @@
+
 def que2122():
 
     n = int(input())
     m = int(input())
 
     dic = {}
+    """
+    说明:计算输入数字的数位和
+    ------------------------------------
+    变量:i
+    ------------------------------------
+    返回:数位之和，eg. i = 10 return 1 + 0 = 1
+    ------------------------------------
+    """
     def sumnum(i):
         return sum(list(map(int,str(i))))
     for i in range(1,n+1):
         dic[i] = sumnum(i)
-
+    
     ordic = sorted(dic.items(),key=lambda x : x[1])
     print(ordic[m-1][0])
     return 0
