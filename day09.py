3ca818d00b013d69403f500f7ad8a3a5d1b7deb6
Zengwenliang0416 <wenliang_zeng416@163.com>
2023-04-07 15:20:31 +0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   day09.py
    	new file:   day10.py

--- day09.py
+++ day09.py
@@ -52,31 +52,138 @@ def que1508():
     print(sum)
 
 
-n = int(input())
-x_label = list(map(int,input().split()))
-y_label = list(map(int,input().split()))
+# n = int(input())
+# x_label = list(map(int,input().split()))
+# y_label = list(map(int,input().split()))
 
-dx = [0,0,-1,1]
-dy = [1,-1,0,0]
-pos = [[]*n]
-start = 0
-for i in range(n):
-    pos.append([])
-    for j in range(n):
-        pos[i].append(start)
-        start += 1
-pos.pop()
+# dx = [0,0,-1,1]
+# dy = [1,-1,0,0]
+# res = []
+# pos = [[]*n]
+# start = 0
+# for i in range(n):
+#     pos.append([])
+#     for j in range(n):
+#         pos[i].append(start)
+#         start += 1
+# pos.pop()
 
-def pd():
+# def pd(nowx,nowy):
+    
+#     return
 
-    return
+# def check(nowx,nowy):
+#     if nowx == n and nowy == n:
+#         return True
+#     else:
+#         return False
 
-def check():
+
+# def DFS(nowx,nowy):
+#     if check(nowx,nowy):
+#         return
+#     else:
+#         for i in range(4):
+#             nextx += dx
+#             nexty += dy
+#             if pd(nextx,nexty):
+#                 DFS(nextx,nexty)
+#             else:
+#                 continue
+
+from collections import *
+def quemikong():
+    mp = []
+    for i in range(30):
+        mp.append(list(map(int,input())))
+    k = ('D','L','R','U')
+    dir = [(1,0),(0,-1),(0,1),(-1,0)]
+    vis = [[0]*50 for i in range(30)]
+    fater = [['']*50 for i in range(30)]
+
+    def dfs(x,y):
+        if x == 0 and y == 0:
+            return
+        if fater[x][y] == 'D': dfs(x-1,y)
+        if fater[x][y] == 'L': dfs(x,y+1)
+        if fater[x][y] == 'R': dfs(x,y-1)
+        if fater[x][y] == 'U': dfs(x+1,y)
+        print(fater[x][y],end="")
+
+    def check(x,y):
+        if x==29 and y==40:
+            return True
+    def pd(x,y):
+        if x<0:
+            return False
+        elif x >= 30:
+            return False
+        elif y < 0:
+            return False
+        elif y >= 50 :
+            return False
+        elif vis[x][y] == 1:
+            return False
+        elif mp[x][y] == 1:
+            return False
+        else:
+            return True
+
+    def bfs(x,y):
+        global fater
+        global vis
+        q = deque()
+        q.append((x,y))
+        vis[x][y] = 1
+        while q:
+            now = q.popleft()
+            if check(now[0],now[1]):
+                return
+            else:
+                for i in range(4):
+                    next = [0,0]
+                    next[0] = now[0] + dir[i][0]
+                    next[1] = now[1] + dir[i][1]
+                    if not pd(next[0],next[1]):
+                        continue
+                    else:
+                        vis[next[0]][next[1]] = 1
+                        q.append((next[0],next[1]))
+                        fater[next[0]][next[1]] = k[i]
+    bfs(0,0)
+    dfs(29,49)
+    print('')
 
 
-    return
 
+def que642():
+    # 跳蚂蚱
+    def insertQueue(q,dir,news,vis):
+        pos = news[1]
+        status = news[0]
+        insertPos = (pos + dir + 9)%9
+        t = list(status)
+        t[pos],t[insertPos] = t[insertPos],t[pos]
+        addStatus = "".join(t)
+        if addStatus not in vis:
+            vis.add(addStatus)
+            q.append((addStatus,insertPos,news[2]+1))
 
-def DFS():
+    jump = [-1,-2,1,2]
+    q = deque()
+    q.append(("012345678",0,0))
+    vis = set()
+    vis.add("012345678")
 
-    return
+    def check(s):
+        if s == '087654321':
+            return True
+        return False
+
+    while q:
+        news = q.popleft()
+        if check(news[0]):
+            print(news[2])
+            break
+        for i in range(4):
+            insertQueue(q,jump[i],news,vis)
---
+++ day10.py
@@ -0,0 +1,54 @@
+def que110():
+    Maxn = 2000000
+    fa = []
+    def init():
+        for i in range(Maxn + 1):
+            fa.append(i)
+    def find(x):
+        if fa[x] == x:
+            return x
+        else:
+            fa[x] = find(fa[x])
+            return fa[x]
+    def merge(x,y):
+        fa[find(x)] = find(y)
+    n,m,k = input().split()
+    Maxn = m*n+100
+    init()
+    for _ in range(k):
+        a,b = int(input().split())
+        merge(a,b)
+    ans = 0
+    for i in range(1,m*n):
+        if fa[i] == i:
+            ans += 1
+    print(ans)
+
+def que364():
+    # 跳石头
+    len, n, m = map(int,input().split())
+    stone = []
+    for i in range(n):
+        stone.append(int(input()))
+    stone.append(len)
+
+    def check(d):
+        num = 0
+        pos = 0
+        for i in range(0,n+1):
+            if (stone[i]-pos <d):
+                num += 1
+            else:
+                pos = stone[i]
+            if num <= m:
+                return True
+            else:
+                return False
+    l,r = 0,len
+    while l < r:
+        mid = (l+r+1)//2
+        if check(mid):
+            l = mid
+        else:
+            r = mid - 1
+    print(l)
